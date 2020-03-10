0695d62b6fb1614e0db16947edc6eee5041f3899
Ajay Ramachandran <dev@ajay.app>
2020-03-10 19:32:54 -0400

    Added header to blog pages

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,37 +1,41 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Header from "../components/header";
+
 import "./blog.scss";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
+    data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
-  return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
-  )
+    const { markdownRemark } = data // data.markdownRemark holds your post data
+    const { frontmatter, html } = markdownRemark
+    return (
+        <Header>
+            <div className="blog-post-container">
+                <div className="blog-post">
+                    <h1>{frontmatter.title}</h1>
+                    <h2>{frontmatter.date}</h2>
+                    <div
+                        className="blog-post-content"
+                        dangerouslySetInnerHTML={{ __html: html }}
+                    />
+                </div>
+            </div>
+        </Header>
+    )
 }
 export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        path
-        title
-      }
+    query($path: String!) {
+        markdownRemark(frontmatter: { path: { eq: $path } }) {
+            html
+            frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                path
+                title
+            }
+        }
     }
-  }
 `
