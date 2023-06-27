d1a1457cef685b53439da081fa4e2ddde6722868
Ajay <dev@ajay.app>
2023-06-27 02:55:56 -0400

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,39 +32,90 @@ module.exports = {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
-                {
-                    resolve: `gatsby-remark-prismjs`,
-                    options: {
-                    classPrefix: "language-",
-                    inlineCodeMarker: null,
-                    aliases: {},
-                    showLineNumbers: true,
-                    noInlineHighlight: false,
-                    languageExtensions: [
-                        {
-                        language: "superscript",
-                        extend: "javascript",
-                        definition: {
-                            superscript_types: /(SuperType)/,
-                        },
-                        insertBefore: {
-                            function: {
-                            superscript_keywords: /(superif|superelse)/,
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            classPrefix: "language-",
+                            inlineCodeMarker: null,
+                            aliases: {},
+                            showLineNumbers: true,
+                            noInlineHighlight: false,
+                            languageExtensions: [
+                                {
+                                    language: "superscript",
+                                    extend: "javascript",
+                                    definition: {
+                                        superscript_types: /(SuperType)/,
+                                    },
+                                    insertBefore: {
+                                        function: {
+                                            superscript_keywords: /(superif|superelse)/,
+                                        },
+                                    },
+                                },
+                            ],
+                            prompt: {
+                                user: "root",
+                                host: "localhost",
+                                global: false,
                             },
+                            escapeEntities: {},
                         },
-                        },
-                    ],
-                    prompt: {
-                        user: "root",
-                        host: "localhost",
-                        global: false,
                     },
-                    escapeEntities: {},
-                    },
-                },
                 ],
             }
         },
-        "gatsby-plugin-feed"
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+                {
+                    site {
+                    siteMetadata {
+                        title
+                        description
+                        siteUrl
+                        site_url: siteUrl
+                    }
+                    }
+                }
+                `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.nodes.map(node => {
+                                return Object.assign({}, node.frontmatter, {
+                                    description: node.excerpt,
+                                    date: node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + node.fields.slug,
+                                    custom_elements: [{ "content:encoded": node.html }],
+                                })
+                            })
+                        },
+                        query: `
+                        {
+                            allMarkdownRemark(
+                            sort: { order: DESC, fields: [frontmatter___date] },
+                            ) {
+                            nodes {
+                                excerpt
+                                html
+                                fields { 
+                                    slug 
+                                }
+                                frontmatter {
+                                    title
+                                    date
+                                }
+                            }
+                            }
+                        }`,
+                        output: "/rss.xml",
+                        title: "Ajay Ramachandran's Blog",
+                    },
+                ],
+            },
+        },
     ],
 };
